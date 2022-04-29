e6f91bb678d23e48aa546292600a4a26b1de8b3f
Noor-26 <nk26982698@gmail.com>
2022-04-29 16:51:15 +0600

    Banner added

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import './Banner.css';
+import campPng from './camping.png'
 const Banner = () => {
     return (
         <div>
+            <div className="img-container">
+            <img src={campPng} className="banner-img" alt="" />
+            </div>
+            <div className="text-container text-white ">
             
+                <h1 className="banner-text " >Equip all your Camping items here</h1>
+                <button className="btn banner-btn px-5 py-3">See items</button>
+               
+            </div>
         </div>
     );
 };
--- src/components/Shared/Navber/Navber.js
+++ src/components/Shared/Navber/Navber.js
@@ -1,9 +1,44 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { FaBeer } from 'react-icons/fa';
+import { HiMenuAlt3 } from 'react-icons/hi';
+import './Navber.css'
 const Navber = () => {
     return (
         <div>
-            
+            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div className="container">
+    <a className="navbar-brand" as={Link} to='/'>Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
+
+      <span className=""><HiMenuAlt3 className="navber-icon"/></span> 
+      
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" as={Link} to='/home'>Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" as={Link} to='/home'>login</a>
+        </li>
+    
+       
+      </ul>
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0" >
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" as={Link} to='/home'>Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" as={Link} to='/home'>login</a>
+        </li>
+    
+       
+      </ul>
+    
+    </div>
+  </div>
+</nav>
         </div>
     );
 };
